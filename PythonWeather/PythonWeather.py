76d3e3b3209f3a309bee7e563b0302937d040a89
Nagrajan23 <c.raj.23@gmail.com>
2018-03-12 14:25:58 -0400

    API added

--- PythonWeather/PythonWeather.py
+++ PythonWeather/PythonWeather.py
@@ -1,1 +1,13 @@
-print("Test Hello")
+import pyowm
+
+#print("Test Hello")
+owm = pyowm.OWM('114c1288a926175bc3b7b768387bc625')
+#observation = owm.weather_at_place('Colombo,LK')
+#w = observation.get_weather()
+fc = owm.three_hours_forecast('Colombo,LK')
+f = fc.get_forecast()
+lst = f.get_weathers()
+print(lst[1])
+print('--------------')
+print(lst[2])
+print(f.get_location())
